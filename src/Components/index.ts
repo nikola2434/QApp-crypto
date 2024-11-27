a43e3439fabffc6cdc88adbee0f12a35a01a1380
Reviakin Nikolay <reviakin223@gmail.com>
2024-11-27 22:05:32 +0300

    add: added asynchronous loading of chunks

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,8 +1,2 @@
 export { Preview } from './Preview';
 export { InfoSMPC } from './InfoSMPC';
-export { Scenario } from './Scenario';
-export { Experts } from './Experts';
-export { Programs } from './Programs';
-export { Career } from './Career';
-export { Map } from './Map';
-export { Organizers } from './Organizers';
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,26 @@
-import { FC } from 'react';
+import { FC, Suspense, lazy } from 'react';
 import './style/index.scss';
-import { Preview, InfoSMPC, Scenario, Experts, Programs, Career, Map, Organizers } from './Components';
+import { Preview, InfoSMPC } from './Components';
+
+// Асинхронная загрузка компонентов
+const Scenario = lazy(() => import('./Components/Scenario').then((module) => ({ default: module.Scenario })));
+const Experts = lazy(() => import('./Components/Experts').then((module) => ({ default: module.Experts })));
+const Programs = lazy(() => import('./Components/Programs').then((module) => ({ default: module.Programs })));
+const Organizers = lazy(() => import('./Components/Organizers').then((module) => ({ default: module.Organizers })));
+const Career = lazy(() => import('./Components/Career').then((module) => ({ default: module.Career })));
+const Map = lazy(() => import('./Components/Map').then((module) => ({ default: module.Map })));
 
 export const Main: FC = () => (
   <div>
     <Preview />
     <InfoSMPC />
-    <Scenario />
-    <Experts />
-    <Programs />
-    <Organizers />
-    <Career />
-    <Map />
+    <Suspense fallback={<div>Загрузка...</div>}>
+      <Scenario />
+      <Experts />
+      <Programs />
+      <Organizers />
+      <Career />
+      <Map />
+    </Suspense>
   </div>
 );
