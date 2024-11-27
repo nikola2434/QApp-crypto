2064d573fc49fb63aefd1c8c24a1e507ab7eadc8
Reviakin Nikolay <reviakin223@gmail.com>
2024-11-27 22:49:43 +0300

    fix: improved image loading

--- src/Components/Preview/Preview.tsx
+++ src/Components/Preview/Preview.tsx
@@ -2,15 +2,19 @@ import { FC } from 'react';
 import style from './Preview.module.scss';
 import { Logos } from './Logos';
 import { Title } from './Title';
-import BgProtocol from '@public/images/bg_preview.png';
-import Logo from '@public/images/logo.png';
+import BgProtocol from '@public/images/bg_preview.svg';
+import Logo from '@public/images/logo.svg';
 
 export const Preview: FC = () => {
   return (
     <div className={style.preview}>
       <div className={style.back}>
-        <img src={Logo} alt="" className={style.logo} draggable={false} />
-        <img src={BgProtocol} alt="" className={style.image} draggable={false} />
+        <div className={style.logo}>
+          <Logo />
+        </div>
+        <div className={style.image}>
+          <BgProtocol />
+        </div>
       </div>
       <Logos />
       <Title />
